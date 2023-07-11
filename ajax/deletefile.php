e785f427b09a48d5a4bc4e042a7a231c9eed9219
DESKTOP-J65IFO1\user <eric22221936@gmail.com>
2023-07-11 15:04:52 +0800

    修正刪除檔案時資料庫抓取錯誤

--- ajax/deletefile.php
+++ ajax/deletefile.php
@@ -2,7 +2,11 @@
 $conn=new mysqli("127.0.0.1","root","","webwork"); //連接資料庫
 
 $index_num=(int)$_POST["ID"];
-$sql="DELETE FROM `file` WHERE `ID` = {$index_num} ";
+$id_name_arr=explode("-",$_POST["ID"]);
+$id_name=$id_name_arr[0];
+$f_name_arr=explode("/",$_POST["filename"]);
+$f_name=$f_name_arr[count($f_name_arr) - 1];
+$sql="DELETE FROM `file` WHERE `filename` = '{$f_name}' AND `ID` = {$id_name}";
 $re=mysqli_query($conn,$sql);
 // function deleteFolder($folderPath) {
 //     if (!is_dir($folderPath)) {
@@ -24,6 +28,8 @@ $re=mysqli_query($conn,$sql);
 //     rmdir($folderPath);
 // }
 
-$folderPath = "../file/{$_COOKIE["account"]}/{$index_num}/{$_POST["filename"]}";
+$filePath = "../{$_POST["filename"]}";
+echo $sql;
 unlink($filePath);
+
 ?>
